0c5d0134ac530d522954b2fb6176e5110e61f219
sajol-cs <rejaulislamsajol@gmail.com>
2025-03-09 01:17:27 +0600

    adding new stratigies

---
+++ 3rd.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+void hello(int a, int b){
+    cout<<"MUL:" <<(a*b) <<endl;
+}
+int main()
+{
+    hello(5,3);
+}
---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Rejaul Islam Sajol's \n";
+    cout<<"My first code";
+    return 0;
+}
