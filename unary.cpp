310db6fc191d5100e3d57c53a416e8c20d9bb137
sajol-cs <rejaulislamsajol@gmail.com>
2025-03-10 22:07:42 +0600

    latest update

---
+++ unary.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int a = 5;
+    int b = a++;
+    cout<<"value of b is: "<<b<<endl;
+    cout<<"Updated value of a is: "<<a;
+    return 0;
+
+}
