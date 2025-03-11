96993e9b3f1783f6d8c75d9f6dd14fd4ea54e94a
sajol-cs <rejaulislamsajol@gmail.com>
2025-03-12 00:02:24 +0600

    latest update

---
+++ Conditional Statements & Loops/canVote.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+   int age;
+   cout<<"enter age: ";
+   cin>>age;
+   if(age>=18){
+    cout<<"Person can Vote";
+   }else{
+    cout<<"Person can't vote.";
+   }
+}
+
---
+++ Conditional Statements & Loops/checkNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+   int n = -1  ;
+   if(n>=0){
+    cout<<"This is a possitive number.";
+    
+    
+    return 0;
+   } else{
+    cout<<"negative";
+}
+
+}
