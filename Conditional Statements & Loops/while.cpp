86cffd994c706a6859affb6447e37fa253e279ea
sajol-cs <rejaulislamsajol@gmail.com>
2025-03-14 21:18:29 +0600

    latest update

---
+++ Conditional Statements & Loops/while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+   int count;
+     count = 1;
+   
+       while (count <=5){
+      cout<<count <<" ";
+      count++;
+    }
+   return 0;
+}
+
